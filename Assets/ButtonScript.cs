b0cc6f3a6c416ac68cce0ed60c3b39d881cc13d3
GeronimoCaporossiGomez <geronimogomezok123@gmail.com>
2024-05-27 15:50:03 -0300

    FINAL_FINAL

---
+++ Assets/ButtonScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonScript : MonoBehaviour
+{
+    public InputField input_nombe;
+    
+
+    // Start is called before the first frame update
+    public void EscribirNombreEnConsola()
+    {
+        string Contra = "mami";
+        string MEME = input_nombe.text;
+        Debug.Log(input_nombe.text);
+        if (MEME == Contra)
+        {
+            Debug.Log("Access Granted");
+
+        }
+        else
+        {
+            Debug.Log("Access Denied");
+        }
+
+    }
+    void Start()
+
+    {
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
